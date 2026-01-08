a5d1418f7906f88f5cea508b30287278ef020497
chikunobou-ai <chikunobou@gmail.com>
2026-01-08 18:13:47 +0900

    テスト

---
+++ component3-2.js
@@ -0,0 +1,11 @@
+const App = {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'Vueとの航海' },
+        { id: 2, title: 'Vueとの旅' },
+        { id: 3, title: 'Vue旅行記' }
+      ]
+    }
+  }
+};
